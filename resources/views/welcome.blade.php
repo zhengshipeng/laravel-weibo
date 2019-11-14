a364b0590a6b3117a3c29518dad26ed8546d0698
zsp <zhengshipeng1992@gmail.com>
2019-11-14 11:12:40 +0000

    数据库配置

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,3 @@
-{{--
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -98,48 +97,4 @@
             </div>
         </div>
     </body>
-</html>
---}}
-        <!DOCTYPE html>
-<html>
-<head>
-    <title>Laravel</title>
-    <style>
-        html, body {
-            height: 100%;
-        }
-
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            display: table;
-            font-weight: 100;
-            font-family: "Helvetica Neue", NotoSansHans-Regular,AvenirNext-Regular,arial,Hiragino Sans GB,"Microsoft Yahei","Hiragino Sans GB","WenQuanYi Micro Hei",sans-serif;
-            color:#777;
-        }
-
-        .container {
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
-
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
-
-        .title {
-            font-size: 66px;
-        }
-    </style>
-</head>
-<body>
-<div class="container">
-    <div class="content">
-        <div class="title">Hello Laravel! - by zsp</div>
-    </div>
-</div>
-</body>
 </html>
