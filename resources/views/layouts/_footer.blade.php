873d58c102341b41a770b1b1e651c90e01c08647
zhengshipeng <370019506@qq.com>
2019-11-15 15:59:45 +0800

    '用户显示页面';

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
-    <img src="image/icon.png" class="brand-icon">
+    <img src="/image/icon.png" class="brand-icon">
     <a href="https://learnku.com/laravel/courses" target="_blank">刻意练习，每日精进</a>
     <div class="float-right">
         <a href="{{ route('about') }}">关于</a>
