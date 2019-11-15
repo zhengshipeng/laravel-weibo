087a4a394dc82316c1fbeeb6c334e37b5e6d7668
zhengshipeng <370019506@qq.com>
2019-11-15 10:54:27 +0800

    '切割头部和尾部视图';

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <img src="image/icon.png" class="brand-icon">
+    <a href="https://learnku.com/laravel/courses" target="_blank">刻意练习，每日精进</a>
+    <div class="float-right">
+        <a href="/about">关于</a>
+    </div>
+</footer>
