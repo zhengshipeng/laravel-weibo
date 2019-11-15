beb2dc266ccad36fb2f165957c37533c30ae7a86
zhengshipeng <370019506@qq.com>
2019-11-15 10:58:57 +0800

    '命名路由';

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,6 +2,6 @@
     <img src="image/icon.png" class="brand-icon">
     <a href="https://learnku.com/laravel/courses" target="_blank">刻意练习，每日精进</a>
     <div class="float-right">
-        <a href="/about">关于</a>
+        <a href="{{ route('about') }}">关于</a>
     </div>
 </footer>
