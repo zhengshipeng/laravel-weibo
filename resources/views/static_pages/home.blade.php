2c546cf4559fc510ff871b9f754f53fb48338547
zhengshipeng <370019506@qq.com>
2019-11-15 10:10:37 +0800

    '初始化样式';

---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.default')
+
+@section('title', '首页')
+
+@section('content')
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在所看到的是 <a href="https://www.baidu.com">laravel入门教程</a> 的示例项目主页。
+        </p>
+        <p>一切将重这里开始。</p>
+        <p><a href="#" class="btn btn-lg btn-success" role="button">现在注册</a></p>
+    </div>
+@stop
