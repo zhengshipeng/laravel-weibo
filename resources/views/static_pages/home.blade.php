fabad7312ddd87dac52b568a2b27b8153008a05d
zhengshipeng <370019506@qq.com>
2019-11-15 11:05:55 +0800

    '用户注册页面';

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -9,6 +9,6 @@
             你现在所看到的是 <a href="https://www.baidu.com">laravel入门教程</a> 的示例项目主页。
         </p>
         <p>一切将重这里开始。</p>
-        <p><a href="#" class="btn btn-lg btn-success" role="button">现在注册</a></p>
+        <p><a href="{{ route('signup') }}" class="btn btn-lg btn-success" role="button">现在注册</a></p>
     </div>
 @stop
