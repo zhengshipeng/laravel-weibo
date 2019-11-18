a8507ded38b6bde5be06c9d589a0ae4d87f3c5c4
zhengshipeng <370019506@qq.com>
2019-11-18 14:44:52 +0800

    '重置密码';

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,5 +25,10 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
+
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
 }
